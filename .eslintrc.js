4028942eb08a1f62db64a145de0ae82bb72ea4ae
Daan Wijns <dw.daanwijns@gmail.com>
2020-10-16 12:01:48 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+        commonjs: true,
+        es2021: true,
+        browser: true
+    },
+    extends: ['plugin:react/recommended', 'eslint:recommended'],
+    parserOptions: {
+        parser: 'babel-eslint',
+        'sourceType': 'module',
+        'ecmaFeatures': {
+            'jsx': true
+        }
+    },
+    'plugins': [
+        'react'
+    ],
+    settings: {
+        react: {
+            'version': 'latest'
+        }
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: ['error', 4],
+        semi: ['error', 'never'],
+        quotes: ['error', 'single'],
+        'comma-dangle': ['error', 'never'],
+        'arrow-parens': ['error', 'as-needed'],
+        'no-underscore-dangle': ['warn', { allowAfterThis: true }],
+        'jsx-quotes': ['error', 'prefer-single'],
+        'react/prop-types': 0
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+}
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <>
+                <div className='App'>
+                    <h1>Helloooooo</h1>
+                </div>
+            </>
+        )
+    }
+}
+
+export default App
