79958a410e038050dd94571c6b82d627e1138eab
Daan Wijns <dw.daanwijns@gmail.com>
2020-11-01 12:08:19 +0100

    use recoil for state management

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import Store from '../services/Store'
+import React from 'react'
+import { useRecoilValue } from 'recoil'
 import SkeletonMovieList from '../skeletons/SkeletonMovieList'
-import EventEmitter from '../services/EventEmitter'
-
 import MovieListItem from './MovieListItem'
+
+import { movies } from '../services/Store'
 
 const MovieList = () => {
 
-    const [movieList, setMovies] = useState(null)
-
-    EventEmitter.subscribe('searchComplete', () => setMovies(Store.getMovies()))
-
-    useEffect(() => {
-        setMovies(Store.getMovies())
-    }, [])
+    const movieList = useRecoilValue(movies)
 
     return (
         <div className='pt-6 mb-10 pb-10 bg-gray-800' >
@@ -23,8 +17,6 @@ const MovieList = () => {
             { !movieList && [1, 2, 3, 4, 5, 6, 7, 8, 9]
                 .map(n => <SkeletonMovieList key={n} />)}
         </div>
-
-
     )
 }
 
